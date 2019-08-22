cc437b8400bd9b41b395b250497b1d38f09808c1
Martin Gordon <mgordon@trinity.edu>
2019-08-22 11:10:29 -0500

    Changed my name.

--- src/main/scala/basics/HelloWorld.scala
+++ src/main/scala/basics/HelloWorld.scala
@@ -5,7 +5,7 @@ This is a basic main for you to start off with.
 */
 object HelloWorld {
 	def main(args: Array[String]): Unit = {
-		println("Hello World!")
+		println("Hello Martin!")
 	}
 	
 	def square(x: Double) = x*x
